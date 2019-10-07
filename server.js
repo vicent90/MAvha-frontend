47e835eac113125e2e8e1aaa49937d21ff192285
Vicente Mazzaro <vicentemazzaro@hotmail.com>
2019-10-07 15:01:41 -0300

    server heroku

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const app = express();
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-};
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//         ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// };
+// // Instruct the app
+// // to use the forceSSL
+// // middleware
+// app.use(forceSSL());
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+
+app.use(express.static(__dirname + '/dist/MAvha-frontend'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 5000, function(){
