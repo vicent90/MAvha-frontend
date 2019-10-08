6447e670c7743b27188b87d6d0e69e2b03116978
Vicente Mazzaro <vicentemazzaro@hotmail.com>
2019-10-08 08:56:03 -0300

    local

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,3 @@
-export const URL_SERVICIOS = 'https://MAvha-backend.herokuapp.com';
-// export const URL_SERVICIOS = 'http://localhost:3000';
+// export const URL_SERVICIOS = 'https://MAvha-backend.herokuapp.com';
+export const URL_SERVICIOS = 'http://localhost:3000';
 export const STATUS = ['pendiente', 'resuelta'];
